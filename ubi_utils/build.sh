07895591a22224a8c2805883be2287cfb7b61fee
jagtapsuresh <91741937+jagtapsuresh@users.noreply.github.com>
2021-10-18 15:30:01 +0530

    Delete ubi_utils directory

--- ubi_utils/build.sh
+++
@@ -1,9 +0,0 @@
-DOCKER_IMAGE_NAME=jboss7.8.0/jboss-fuse-builder-image
-DOCKER_IMAGE_VERSION=latest
-
-docker rmi --force=true ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
-podman build --force-rm=true --rm=true -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
-echo =========================================================================
-echo Docker image is ready.  Try it out by running:
-echo     docker run -d --rm -ti -P ${DOCKER_IMAGE_NAME}
-echo =========================================================================
